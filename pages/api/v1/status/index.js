6ce6560f103e27a885ce9648e4f7b37790f3225a
Matheus Rocha <62039446+matheusrch@users.noreply.github.com>
2026-07-04 11:07:31 -0300

    add `jsconfig.json`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js"; // também é posível usar /infra para ir direto ao root path (e aí não precisa do jsconfig.json)
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 2 AS sum;");
